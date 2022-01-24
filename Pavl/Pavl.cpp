f31f2d8407bdf154dc1ddaab06bb25446f690cb3
Sara894 <t48250klo@gmail.com>
2022-01-24 20:51:04 +0300

    реализация в среде Visual C++ 6.0

--- Pavl/Pavl.cpp
+++ Pavl/Pavl.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <windows.h>
 //Т.А. Павловская, Ю.А. Щупак
 //С/С++ Структурное программирование. Практикум
 //Семинар 1. Линейные программы.
@@ -8,14 +9,22 @@
 //C = 5/9(F-32)
 //где С - температура по Цельсию, а F - по Фаренгейту.
 using namespace std;
+
+char* Rus(const char* text);
 int main()
 {
     float fahr, cels;
-    cout << endl << " Введите температуру по Фаренгейту" << endl;
+    cout << endl << Rus(" Введите температуру по Фаренгейту") << endl;
     cin >> fahr;
     cels = 5 / 9 * (fahr - 32);
-    cout << " По Фаренгейту: " << fahr << ", в градусах Цельсия: "
+    cout << Rus(" По Фаренгейту: ") << fahr << Rus(", в градусах Цельсия: ")
         << cels << endl;
     return 0;
+}
+
+char bufRus[256];
+char* Rus(const char* text) {
+    CharTo0em(text, bufRus);
+    return bufRus; 
 }
 
