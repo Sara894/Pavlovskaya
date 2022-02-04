3afcd7c56505b9751bde56364d0666d6579031d5
Sara894 <t48250klo@gmail.com>
2022-02-04 20:10:27 +0300

    Задания к семинару, вариант 11

--- Pavl/Pavl.cpp
+++ Pavl/Pavl.cpp
@@ -1,34 +1,20 @@
 ﻿#include <iostream>
-#include <math.h>
-#include <float.h>
 //Т.А. Павловская, Ю.А. Щупак
 //С/С++ Структурное программирование. Практикум
 //Семинар 2. Разветвляющиеся программы. Циклы.
-//Задача 2.5. Вычисление суммы ряда.
-//Написать программу вычисления значения функции Ch x (гиперболический косинус)
-//с помощью бесконечного ряда Тейлора с точностью epsilon по формуле
-//y = 1 + x^2/2! + ... + x^2n/2n! + ...
+//Task 1. Variant 11. 
+//Calculate and display on the screen in the form of a table the values 
+//of the function in the interval  from Xbegin to Xend in increments of 1 
 using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Ru");
-	const int MaxIter = 500;//максимально допустимое количество итераций
-	double x, eps = DBL_EPSILON;//максимально допустимая точность
-	cout << "\nEnter a argument : " << endl;
-	cin >> x;
-	double ch = 1, y = ch;//first term of series and initial value of sum
-	int n = 0;
-	for (n; fabs(ch) > eps && n < MaxIter; n++) {
-		ch *= x * x / ((2 * n + 1) * (2 * n + 2));//another member of the series
-		y += ch;//adding series member to the sum
-	}
-	if (n < MaxIter) {
-		cout << "function value: " << y << " for x = " << x << endl;
-		cout << " calculated after " << n << " iterations " << endl;
-		cout << " calculated with library math.h function cosh(x) = " << cosh(x) << endl;
-	}
-	else cout << "\n The series diverges!" << endl;
+	double x,a, b, c;
+	cout << "Enter x,a,b,c : ";
+	cin >> x >> b >> c;
+	if (x < 1 && c != 0) cout << (a * x * x) + (b / c) << endl;
+	else if (x > 15 && c == 0) cout << (x - a) / (x - c) * (x - c) << endl;
+	else cout << (x * x) / (c * c) << endl;
 	return 0;
 }
 
