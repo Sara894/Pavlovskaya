6d74c4ae7e78e26e37c00b9ed9c42e3923cdf7f9
Sara894 <t48250klo@gmail.com>
2022-02-04 09:56:02 +0300

    Задача 2.5. вычисление суммы ряда

--- Pavl/Pavl.cpp
+++ Pavl/Pavl.cpp
@@ -1,37 +1,36 @@
 ﻿#include <iostream>
-#include <stdio.h>
-#include <conio.h>
+#include <math.h>
 //Т.А. Павловская, Ю.А. Щупак
 //С/С++ Структурное программирование. Практикум
 //Семинар 2. Разветвляющиеся программы. Циклы.
-//Задача 2.5. Таблица значений функции.
-//Написать программу печати таблицы значений функции
-//    t , x < 0
-//y = tx, 0 <= x < 10
-//    2t, x >= 10
-//для аргумента, изменяющегося в заданных пределах с заданным шагом. Если 
-//t>100, должны выводиться целые значения функции
-
+//Задача 2.5. Вычисление суммы ряда.
+//Написать программу вычисления значения функции Ch x (гиперболический косинус)
+//с помощью бесконечного ряда Тейлора с точностью epsilon по формуле
+//y = 1 + x^2/2! + ... + x^2n/2n! + ...
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Ru");
-	double Xn, Xk, dX, t, y;
-	cout << "Enter Xn, Xk, dX, t " << endl;
-	cin >> Xn >> Xk >> dX >> t;
-	cout << "--------------------" << endl;
-	cout << "|      X     |      Y     |\n" << endl;
-	cout << "--------------------" << endl;
-	for (double x = Xn; x < Xk; x += dX)
-	{
-		if (x < 0) y = t;
-		if (x >= 0 && x < 10) y = t * x;
-		if (x >= 10) y = 2*t;
-		if (t > 100) cout << "    " << x << "    " << static_cast<int>(y) << endl;
-		else cout << "    " << x << "    " << y << endl;
+	const int MaxIter = 500;//максимально допустимое количество итераций
+	double x, eps;
+	cout << "\nEnter a argument and precision: " << endl;
+	cin >> x >> eps;
+	bool done = true;//признак достижимости точности
+	double ch = 1, y = ch;//first term of series and initial value of sum
+	int n = 0;
+	for (n; fabs(ch) > eps; n++) {
+		ch *= x * x / ((2 * n + 1) * (2 * n + 2));//another member of the series
+		y += ch;//adding series member to the sum
+		if (n > MaxIter) {
+			cout << "\nThe series diverges!";
+			done = false; break;
+		}
 	}
-	cout << "------------------------" << endl;
+	if (done) {
+		cout << "function value: " << y << " for x = " << x << endl;
+		cout << " calculated after " << n << " iterations " << endl;
+	}
 	return 0;
 }
 
