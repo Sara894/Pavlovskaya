fbf258071f2fcbca7daee1e4c18cd88c63cc3e83
Sara894 <t48250klo@gmail.com>
2022-02-05 14:37:03 +0300

    решение

--- Pavl/Pavl.cpp
+++ Pavl/Pavl.cpp
@@ -5,16 +5,35 @@
 //Task 1. Variant 11. 
 //Calculate and display on the screen in the form of a table the values 
 //of the function in the interval  from Xbegin to Xend in increments of 1 
+//The function F must take a real value if the expression (Ac and Bc) MOD2 Sc is not zero,
+//and an integer value otherwise.
+//The integer parts of the values a,b,c are denoted by Ac, Vc, Sc.
+//The operations And and MOD2 (addition modulo 2) are bitwise.
+//Enter the values a,b,c, Xbegin,Xend, dX from the keyboard.
 using namespace std;
 
 int main()
 {
 	double x,a, b, c;
-	cout << "Enter x,a,b,c : ";
-	cin >> x >> b >> c;
-	if (x < 1 && c != 0) cout << (a * x * x) + (b / c) << endl;
-	else if (x > 15 && c == 0) cout << (x - a) / (x - c) * (x - c) << endl;
-	else cout << (x * x) / (c * c) << endl;
+	cout << "Enter a,b,c : ";
+	cin >> a >> b >> c;
+	double Xbegin, Xend, dX;
+	cout << "Enter Xbegin, Xend, dX : ";
+	cin >> Xbegin >> Xend >> dX;
+	x = Xbegin;
+	cout << "|    x    |     F     |" << endl;
+	while (x <= Xend){
+		int t = ((int)a & (int)b) ^ (int)c;
+		double F;
+		if (x < 1 && c != 0) { F = (a * x * x) + (b / c); }
+		else if (x > 1.5 && c == 0) { F = (x - a) / (x - c) * (x - c); }
+		else { F = (x * x) / (c * c); }
+		if(t == 0)
+			cout << "|  " << x << "   |   " << static_cast<int>(F) << "|" << endl;
+		else
+			cout << "|  " << x << "   |   " << F << "|" << endl;
+		x += dX;
+	}
 	return 0;
 }
 
