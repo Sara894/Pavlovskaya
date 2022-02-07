09d86704a3d81f1459535568ce5ba54a8e444603
Sara894 <t48250klo@gmail.com>
2022-02-07 21:08:16 +0300

    Program for output and input  of array elements

--- Pavl/Pavl.cpp
+++ Pavl/Pavl.cpp
@@ -3,29 +3,21 @@
 #include <math.h>
 //Т.А. Павловская, Ю.А. Щупак
 //С/С++ Структурное программирование. Практикум
-//Workshop 3. One-dimensional arrays and pointers.
-//Task 3.1. Number of elements between minimum and maximum
-//Write a program that, for an integer array of 10 elements, determines 
-//how many positive elements are located between its maximum and minimum elements.  ll
+//Write the program that, display on the screen all numbers from the array
 
 using namespace std;
 
 int main()
 {
-	const int n = 5;
-	int a[n] = { -1,-1,-1,-1,-1};
-	int i, imax, imin, count;
-	for (i = imax = imin = 0; i < n; i++) {
-		if (a[i] > a[imax]) imax = i;
-		if (a[i] < a[imin]) imin = i;
-	}
-	cout << "\n\t max = " << a[imax] << " min = " << a[imin];//debugging
-	int ibeg = imax < imin ? imax : imin;
-	int iend = imax < imin ? imin : imax;
-	cout << "\n\t ibeg= " << ibeg << " iend= " << iend;
-	for (count = 0, i = ibeg + 1; i < iend; i++) {
-		if (a[i] > 0) count++;
-	}
-	cout << "Number of positive: " << count << endl;
+	const int n = 1000;
+	int a[n];
+	cout << "\nPlease, enter the number of elements in the array: " << endl;
+	int num_elmnts;
+	cin >> num_elmnts;
+	if (num_elmnts > n)
+		cout << "The number of elements is bigger than default size of array" << endl;
+	cout << "Please, enter all the elements of the array:" << endl;
+	for (int i = 0; i < num_elmnts; i++) cin >> a[i];
+	for (int i = 0; i < num_elmnts; i++) cout << a[i] << " ";
 	return 0;
 }
